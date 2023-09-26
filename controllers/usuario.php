9b3935e95f359144648f6b72d7f804a4cb97bc74
Gabriel Siriani <50271636+Gn112@users.noreply.github.com>
2023-09-26 09:00:24 -0300

    Update usuario.php

--- controllers/usuario.php
+++ controllers/usuario.php
@@ -7,5 +7,9 @@ function insert($pdo, $nome, $email)
     $sql = "INSERT INTO pessoa (usuario, email, senha)
             VALUES (:usuario, :email, :senha)";
     $stmt = $pdo_>prepare($sql);
+    $stmt->bindParam(':nome', $nome, PDO::PARAM_STR);
+    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+    $stmt->bindParam(':senha', $senha, PDO::PARAM_STR);
+    return $stmt->execute();
 }
 ?>
